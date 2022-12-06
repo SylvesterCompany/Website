19b3a2d294d5d7f968b3a19a8643c242058a47b2
F1D0 <vincent.cohadon@outlook.com>
2022-12-06 12:10:50 +0100

    Changes by delphi

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
     <section>
-
+        <span>Copyright &copy 2022 - Vincent Cohadon, Loukas Changeux, Alonso Quispe et Simon Breil</span>
     </section>
 </footer>
--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,12 @@
 <header>
     <nav>
-
+        <ul>
+            <li><a href="credits.php">Crédits</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="about.php">À propos</a></li>
+        </ul>
+        <div id="univ-aside">
+            <span>Réalisé dans le cadre d'un projet universitaire</span>
+        </div>
     </nav>
 </header>
