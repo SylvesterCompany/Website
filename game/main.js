58d7d4e2ccee8f4a87f791c81dd7b959a9418b47
F1D0 <vincent.cohadon@outlook.com>
2022-12-06 12:10:44 +0100

    Changes by delphi

--- game/main.js
+++ game/main.js
@@ -1,16 +1,18 @@
-import MenuScene from "./scenes/menu";
-import GameScene from "./scenes/menu";
+import MenuScene from "./scenes/menu.js";
+import GameScene from "./scenes/game.js";
 
+const canvas = document.getElementById('game');
+console.log(canvas)
 
 const config = {
-    type: Phaser.AUTO,
+    type: Phaser.WEBGL,
     width: 320, // 20 tiles
     height: 208, // 13 tiles
-
+    canvas: canvas,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: {y: 300},
             debug: false
         }
     },
