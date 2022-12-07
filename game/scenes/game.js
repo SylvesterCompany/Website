4293f46a1655fe1ad272d76584aac31db0eaf38d
lchangeu <loukas.changeux@etudiant.univ-lr.fr>
2022-12-07 21:53:28 +0100

    Affichage de la map fait

--- game/scenes/game.js
+++ game/scenes/game.js
@@ -7,20 +7,20 @@ export default class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('Tilesset', 'game/tiles/tileset.png')
-        this.load.image('Background', 'game/tiles/backgrounds.png')
+        this.load.image('Tileset', 'game/tiles/tileset.png')
+        this.load.image('backgrounds', 'game/tiles/backgrounds.png')
         // load the JSON file
         this.load.tilemapTiledJSON('map_tiles', 'game/tiles/Map-test.json')
     }
 
     create() {
         const map = this.add.tilemap('map_tiles')
-        const back = map.addTilesetImage('background')
-        const tileset = map.addTilesetImage('Terrain')
+        const back = map.addTilesetImage('backgrounds')
+        const tileset = map.addTilesetImage('Tileset')
 
-        map.createLayer('Background', back)
-        map.createLayer('Calque de Tuiles 1', tileset)
-        map.createLayer('Front-props', tileset)
+        map.createLayer('Calque de Tuiles 1', back)
+        map.createLayer('Calque de Tuiles 2', tileset)
+        map.createLayer('Calque de Tuiles 3', tileset)
     }
 
     update() {
