b73f1205321895c75f0504017e7e008ca28650a6
F1D0 <vincent.cohadon@outlook.com>
2022-12-09 22:50:55 +0100

    Switch from js to php to generate copyright names

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,3 @@
-/**
- * Generates the copyright including the current year as well as the authors in a random order
- */
-function generateCopyright(authors) {
-    const year = (new Date()).getFullYear();
-    const textElement = document.getElementById("copyright");
-    const authorAmount = authors.length;
-
-    textElement.innerHTML = "Copyright &copy " + year + " - ";
-
-    // Adds the names
-    for (let i = 0; i < authorAmount; i++) {
-        const randomAuthor = authors.splice(Math.round(Math.random() * (authors.length - 1)), 1);
-        textElement.innerHTML += randomAuthor;
-
-        if (i === authorAmount - 2) { // Second to last iteration
-            textElement.innerHTML += " et ";
-        } else if (i < authorAmount - 1) {
-            textElement.innerHTML += ", ";
-        }
-    }
-}
-
 function switchToCanvas() {
     const titleContainer = document.getElementById("title-container");
     const gameContainer = document.getElementById("game-container");
@@ -30,10 +7,6 @@ function switchToCanvas() {
 }
 
 // Main program
-
-const authors = ["Simon Breil", "Vincent Cohadon", "Loukas Changeux", "Alonso Quispe"];
 const playBtn = document.getElementById("play-btn");
-
-generateCopyright(authors);
 
 playBtn.addEventListener("click", switchToCanvas);
--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,11 @@
+<?php
+$names = ['Vincent Cohadon', 'Loukas Changeux', 'Simon Breil', 'Alonso Quispe'];
+shuffle($names);
+$last = array_pop($names);
+$copyright = 'Copyright &copy ' . date('Y') . ' Sylvester - ' . implode(', ', $names) . ' et ' . $last;
+?>
 <footer>
-    <span id="copyright"></span>
+    <span id="copyright">
+        <?=$copyright;?>
+    </span>
 </footer>
