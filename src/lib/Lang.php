1c902e57f7e8941cbdbc69e20d2e49d2503f8609
F1D0 <vincent.cohadon@outlook.com>
2022-12-14 09:45:55 +0100

    Lang

---
+++ src/lib/Lang.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace lib;
+
+class Lang
+{
+    public const DIRECTORY = __DIR__ . '/../../lang';
+
+    public const ALL = [
+        'en' => 'en-US',
+        'es' => 'es-ES',
+        'fr' => 'fr-FR',
+    ];
+
+    public function load() : void
+    {
+        $language = http_negotiate_language(self::ALL);
+        echo $language;
+    }
+}
