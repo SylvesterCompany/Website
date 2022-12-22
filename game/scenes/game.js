9091e3294d619083e27747a289c84d6db3a9e474
lchangeu <loukas.changeux@etudiant.univ-lr.fr>
2022-12-22 11:05:08 +0100

    code plus lisible.

--- game/scenes/game.js
+++ game/scenes/game.js
@@ -6,21 +6,21 @@ export default class GameScene extends Phaser.Scene {
     decors;
     background;
     plateformes;
+    gameover;
 
     constructor() {
         super('GameScene')
+        this.gameover = false;
     }
 
     preload() {
         // Player's textures
-
         this.load.image("player", "assets/img/player/sylvester_contour.png");
         this.load.image("player-left", "assets/img/player/sylvester_contour_left.png");
         this.load.spritesheet("player-running", "assets/img/player/sylvester_anim.png", { frameWidth: 24, frameHeight: 16 });
         this.load.spritesheet("player-running-left", "assets/img/player/sylvester_anim_left.png", { frameWidth: 24, frameHeight: 16 });
 
-        // --
-
+        // Map's textures
         this.load.image('tileset', 'game/tiles/tileset.png')
         this.load.image('backgrounds', 'game/tiles/backgrounds.png')
 
@@ -29,27 +29,40 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create() {
+        this.createWorld();
+        this.createPlayer();
+        this.createCamera();
+        this.physics.add.collider(this.player, this.plateformes);
+
+        // DEBUG
+        // this.plateformes.renderDebug(this.add.graphics());
+    };
+
+    createPlayer(){
+        this.player = new Player(this, 100, 100);
+        this.player.visible = true;
+    };
+
+    createWorld(){
+        //Add Tiles set
         const map = this.add.tilemap('map_tiles')
         const back = map.addTilesetImage('backgrounds')
         const tileset = map.addTilesetImage('tileset')
 
+        //Create Layers
         this.background = map.createLayer('Background', back)
         this.plateformes = map.createLayer('Plateformes', tileset)
         this.decors = map.createLayer('Decors', tileset)
-        this.player = new Player(this, 100, 100);
-        this.player.visible = true;
 
+        //Add physics
         this.plateformes.setCollisionByProperty({ estSolide: true });
-        this.physics.add.collider(this.player, this.plateformes);
-
-        // DEBUG
-
-        // this.plateformes.renderDebug(this.add.graphics());
-
-        this.physics.world.setBounds(0,0,40*16,13*16,)
-        this.cameras.main.setBounds(0,0,40 * 16,13 * 16);
-        this.cameras.main.startFollow(this.player, true);
-    }
+    };
+     createCamera(){
+         //Add camera
+         this.physics.world.setBounds(0,0,40*16,13*16)
+         this.cameras.main.setBounds(0,0,40 * 16,13 * 16);
+         this.cameras.main.startFollow(this.player, true);
+     };
 
     update() {
         this.cursors = this.input.keyboard.createCursorKeys(); // Retrieves the keys
