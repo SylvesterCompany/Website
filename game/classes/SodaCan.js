982c20bfb31f7cfbb05d6abccbd7929d4e88af8a
simon <simon.breil@etudiant.univ-lr.fr>
2022-12-28 23:15:30 +0100

    Implémentation des canettes (finale)

--- game/classes/SodaCan.js
+++ game/classes/SodaCan.js
@@ -2,12 +2,16 @@ export default class SodaCan extends Phaser.Physics.Arcade.Sprite {
     static SPRITE_WIDTH = 16;
     static SPRITE_HEIGHT = 16;
 
-    constructor(scene, x, y) {
+    archiveId;
+
+    constructor(scene, x, y, archiveId) {
         super(scene, x, y, "soda_can");
 
         // Adds the soda can to the scene
         scene.add.existing(this);
         scene.physics.add.existing(this);
+
+        this.archiveId = archiveId;
 
         this.body.immovable = true;
         this.body.allowGravity = false;
--- game/scenes/game.js
+++ game/scenes/game.js
@@ -69,10 +69,10 @@ export default class GameScene extends Phaser.Scene {
         const sodacans = this.map.objects.find(object => object.name === "sodacans").objects;
 
         for (const sc of sodacans) {
-            // console.log(sc)
-            const newSodacan = new SodaCan(this, sc.x, sc.y);
+            const archiveId = sc.properties.find(property => property.name === "archiveId").value;
+            const newSodacan = new SodaCan(this, sc.x, sc.y, archiveId);
 
-            this.physics.add.overlap(this.player, newSodacan, null, null, this);
+            this.physics.add.overlap(this.player, newSodacan, this.collectArchive, null, this);
 
             this.sodacans = [...this.sodacans, newSodacan];
         }
@@ -157,12 +157,14 @@ export default class GameScene extends Phaser.Scene {
         }
     };
 
-    collectArchive(id) {
-        this.archiveCollection.collect(id);
+    collectArchive(player, sodacan) {
+        sodacan.destroy();
+
+        this.archiveCollection.collect(sodacan.archiveId);
 
         this.physics.pause();
 
-        openArchive(this.archiveCollection.getArchive(id), () => {
+        openArchive(this.archiveCollection.getArchive(sodacan.archiveId), () => {
             this.physics.resume();
         });
     }
